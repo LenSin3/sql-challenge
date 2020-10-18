e2f072e13947bc4663381f1d30df4369bc579edb
LenSin3 <lenpk3@yahoo.co.uk>
2020-10-18 09:41:04 -0700

    queries of employees_db database

---
+++ EmployeeSQL/employees_db_queries.sql
@@ -0,0 +1,7 @@
+-- Check if tables were loaded correctly and without errors
+SELECT * FROM departments LIMIT 1;
+SELECT * FROM titles LIMIT 1;
+SELECT * FROM salaries LIMIT 1;
+SELECT * FROM dept_manager LIMIT 1;
+SELECT * FROM dept_emp LIMIT 1;
+SELECT * FROM employees LIMIT 1;
