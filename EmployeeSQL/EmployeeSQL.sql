3043f7369e11b989d89c2cc143c350b8e09c428a
LenSin3 <lenpk3@yahoo.co.uk>
2020-10-18 09:35:37 -0700

    successfully created schema and imported tables into employees_db database

--- EmployeeSQL/EmployeeSQL.sql
+++ EmployeeSQL/EmployeeSQL.sql
@@ -1,9 +1,14 @@
 -- SCHEMA FOR DATABASE employees_db
 
--- TABLE 1: departments
--- Drop table if it exists
+-- Drop tables if they exist
 DROP TABLE departments;
+DROP TABLE titles;
+DROP TABLE salaries;
+DROP TABLE dept_manager;
+DROP TABLE dept_emp;
+DROP TABLE employees;
 
+-- TABLE 1: departments
 -- Create departments table
 CREATE TABLE departments (
 	dept_no VARCHAR(30) PRIMARY KEY,
@@ -12,9 +17,6 @@ CREATE TABLE departments (
 
 	
 -- TABLE 2: titles
--- Drop table if it exists
-DROP TABLE titles;
-
 -- Create table titles
 CREATE TABLE titles (
 	title_id VARCHAR(30) PRIMARY KEY,
@@ -22,9 +24,6 @@ CREATE TABLE titles (
 );
 
 -- TABLE 3: salaries
--- Drop table if exists
-DROP TABLE salaries;
-
 -- Create table salaries
 CREATE TABLE salaries (
 	emp_no INT PRIMARY KEY,
@@ -33,41 +32,33 @@ CREATE TABLE salaries (
 
 
 -- TABLE 4: dept_manager
--- Drop table if it exists
-DROP TABLE dept_manager;
-
 -- Create table dept_manager
 CREATE TABLE dept_manager (
-	dept_no NOT NULL,
-	emp_no NOT NULL,
-	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-	FOREIGN KEY (emp_no) REFERENCES salaries (emp_no)
+	dept_no VARCHAR(30),
+	emp_no INT,
+	FOREIGN KEY(dept_no) REFERENCES departments(dept_no),
+	FOREIGN KEY(emp_no) REFERENCES salaries(emp_no)
 );
 
 -- TABLE 5: dept_emp
--- Drop table if it exists
-DROP TABLE dept_emp;
-
 -- Create table dept_emp
 CREATE TABLE dept_emp (
-	emp_no NOT NULL,
-	dept_no NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES salaries (emp_no),
-	FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
+	emp_no INT,
+	dept_no VARCHAR(30),
+	FOREIGN KEY(emp_no) REFERENCES salaries(emp_no),
+	FOREIGN KEY(dept_no) REFERENCES departments(dept_no)
 );
 
 -- TABLE 6: employees
--- Drop table if it exists
-DROP TABLE employees;
-
 -- Create table employees
 CREATE TABLE employees (
-	emp_no NOT NULL,
-	emp_title VARCHAR(30) NOT NULL,
+	emp_no INT,
+	emp_title VARCHAR(30),
 	birth_date DATE NOT NULL,
 	first_name VARCHAR(30) NOT NULL,
 	last_name VARCHAR (30) NOT NULL,
 	sex VARCHAR (30) NOT NULL,
 	hire_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES salaries (emp_no)
+	FOREIGN KEY(emp_no) REFERENCES salaries(emp_no),
+	FOREIGN KEY(emp_title) REFERENCES titles(title_id)
 );
